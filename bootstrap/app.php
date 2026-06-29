e572524f40487ca961c470b4f8141d3f4f8a2d42
Fabrizio Garcia <fabriziogarciaalva18@gmail.com>
2026-06-29 02:41:56 -0500

    V 0.11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\CheckIsDelivery; // 👈 IMPORTAR EL MIDDLEWARE
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,9 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // 👇 REGISTRAR EL ALIAS DEL MIDDLEWARE AQUÍ
+        // 👇 REGISTRO DEL ALIAS
         $middleware->alias([
-            'is_delivery' => CheckIsDelivery::class,
+            'is_delivery' => 'App\\Http\\Middleware\\CheckIsDelivery',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
